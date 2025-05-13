aa2d0d2809cf3b21ca39252422e39033068fb32d
Nguyen Duc Thinh <tng204@fs0.das5.cs.vu.nl>
2025-05-13 20:06:01 +0200

    second train

--- train_job.sh
+++ train_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=logs/%x_%j.out           # Save output to logs/jobname_jobid.out
 #SBATCH --error=logs/%x_%j.err            # Save errors to logs/jobname_jobid.err
 #SBATCH --time=12:00:00                  # Run time (hh:mm:ss)
-#SBATCH --partition=defq                  # Default queue (has GPUs)
+#SBATCH --partition=proq                  # Default queue (has GPUs)
 #SBATCH --gres=gpu:1                      # Request 1 GPU
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8                 # Adjust CPU cores if needed
