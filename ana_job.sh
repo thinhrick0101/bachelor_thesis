b14a062125ddc3f2b32e811f46947241c312058a
thinhrick0101 <thinhrickgiadinh@gmail.com>
2025-05-21 12:45:05 +0200

    Refactor stable_char_transformer.py to improve the handling of custom attention mechanisms in EnhancedCharTransformer and EnhancedTransformerBlock classes. Updated parameters for attention_class and attention_kwargs to enhance model configurability and flexibility in attention selection.

---
+++ ana_job.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --job-name=analyze_attention      # Name of your job
+#SBATCH --output=logs/%x_%j.out           # Save output to logs/jobname_jobid.out
+#SBATCH --error=logs/%x_%j.err            # Save errors to logs/jobname_jobid.err
+#SBATCH --time=120:00:00                  # Run time (hh:mm:ss)
+#SBATCH --partition=proq                  # Default queue (has GPUs)
+#SBATCH --gres=gpu:1                      # Request 1 GPU
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8                 # Adjust CPU cores if needed
+
+# Load CUDA
+module load cuda11.3/toolkit
+
+# Activate your conda environment
+source /var/scratch/$USER/anaconda3/etc/profile.d/conda.sh
+conda activate mltrain
+
+# Go to your code directory
+cd /var/scratch/$USER/thesis/bachelor_thesis
+
+# Clear CUDA cache before running
+python -c "import torch; torch.cuda.empty_cache()"
+
+# Run your training script (adjust paths/configs as needed)
+python -u analyze_attention.py
